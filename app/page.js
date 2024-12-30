f8e27c27ce10e0f02d4930425c1b63d734f09fa6
sumukhj1219 <sumukhjoshi75@gmail.com>
2024-12-30 20:53:25 +0530

    added feature

--- app/page.js
+++ app/page.js
@@ -1,22 +1,14 @@
-import Head from 'next/head';
+import React from 'react';
+import { metadata as generatedMetadata } from '@/component/GenerateMetaData';
 
-export default function Page() {
+export const metadata = generatedMetadata;
+
+const Page = () => {
   return (
-    <>
-      <Head>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="High-Performance Image & Video Delivery at Scale in Next.js" />
-        <meta name="twitter:description" content="Next Cloudinary" />
-        <meta name="twitter:image" content="https://res.cloudinary.com/demo/image/upload/v1234567890/sample.jpg" />
-        <meta property="og:image" content="https://res.cloudinary.com/demo/image/upload/v1234567890/sample.jpg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:title" content="High-Performance Image & Video Delivery at Scale in Next.js" />
-        <meta property="og:description" content="Next Cloudinary" />
-      </Head>
-      <div>
-        {/* Page content */}
-      </div>
-    </>
+    <div>
+      Hello
+    </div>
   );
-}
+};
+
+export default Page;
