cf76efe9f9a0c109c75cf26fa672c36131590881
sumukhj1219 <sumukhjoshi75@gmail.com>
2024-12-30 19:57:20 +0530

    init

--- app/page.js
+++ app/page.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { metadata } from '@/component/GenerateMetaData';
+import Head from 'next/head';
 
-export const getMetadata = () => metadata;
-
-const page = () => {
+export default function Page() {
   return (
-    <div>
-      Hello
-    </div>
-  )
+    <>
+      <Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="High-Performance Image & Video Delivery at Scale in Next.js" />
+        <meta name="twitter:description" content="Next Cloudinary" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/demo/image/upload/v1234567890/sample.jpg" />
+        <meta property="og:image" content="https://res.cloudinary.com/demo/image/upload/v1234567890/sample.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:title" content="High-Performance Image & Video Delivery at Scale in Next.js" />
+        <meta property="og:description" content="Next Cloudinary" />
+      </Head>
+      <div>
+        {/* Page content */}
+      </div>
+    </>
+  );
 }
-
-export default page
